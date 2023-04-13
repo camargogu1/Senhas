cfcc9fc6d64939733b18d349c7d4542e2c969661
Guilherme Colonhese Camargo <104094315+camargogu1@users.noreply.github.com>
2023-04-13 09:42:13 -0300

    bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Login from './Login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Rotas from './Rotas';
 const root = ReactDOM.createRoot(document.getElementById('root'));
